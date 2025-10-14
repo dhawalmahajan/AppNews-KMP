248c835dc7caa40d88aeb93b5b86ac1f52b024f5
Dhawal Mahajan <dhawal.mahajan1@gmail.com>
2025-10-14 12:03:49 +0530

    add viewmodel and resource component

---
+++ composeApp/src/commonMain/kotlin/com/example/appnews/ui/common/ShimmerEffect.kt
@@ -0,0 +1,18 @@
+package com.example.appnews.ui.common
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun ShimmerEffect() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        CircularProgressIndicator(
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
--- composeApp/src/commonMain/kotlin/com/example/appnews/ui/headline/HeadlineScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/appnews/ui/headline/HeadlineScreen.kt
@@ -1,10 +1,23 @@
 package com.example.appnews.ui.headline
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.appnews.ui.common.ArticleListScreen
-import com.example.appnews.utils.articles
+import com.example.appnews.ui.common.EmptyContent
+import com.example.appnews.ui.common.ShimmerEffect
 
 @Composable
 fun HeadlineScreen() {
-    ArticleListScreen(articleList = articles)
+    val headlineViewModel = viewModel { HeadlineViewModel() }
+    val uiState by headlineViewModel.newsStateFlow.collectAsState()
+    uiState.DisplayResult(onIdle = {}, onLoading = {
+        ShimmerEffect()
+    }, onSuccess = {
+        ArticleListScreen(articleList = it)
+    }, onError = {
+        EmptyContent(it)
+    })
+
 }
