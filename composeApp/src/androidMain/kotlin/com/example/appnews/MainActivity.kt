0b55013f640e3ba736712f5181276095aef18242
Dhawal Mahajan <dhawal.mahajan1@gmail.com>
2025-10-18 17:46:17 +0530

    add drawable xml files

--- composeApp/src/androidMain/kotlin/com/example/appnews/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/appnews/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.appnews.utils.setActivityProvider
 
 class MainActivity : ComponentActivity() {
@@ -13,6 +14,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setActivityProvider { this }
+        installSplashScreen()
         setContent {
             App()
         }
--- composeApp/src/desktopMain/kotlin/com/example/appnews/Main.kt
+++ composeApp/src/desktopMain/kotlin/com/example/appnews/Main.kt
@@ -5,7 +5,10 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import appnews.composeapp.generated.resources.Res
+import appnews.composeapp.generated.resources.logo
 import com.example.appnews.di.initKoin
+import org.jetbrains.compose.resources.painterResource
 
 fun main() = application {
     initKoin()
@@ -14,7 +17,8 @@ fun main() = application {
         title = "AppNews",
         state = WindowState(
             position = WindowPosition(Alignment.Center),
-        )
+        ),
+        icon = painterResource(Res.drawable.logo)
     ) {
 //        window.minimumSize = Dimension(1200, 768)
         App()
